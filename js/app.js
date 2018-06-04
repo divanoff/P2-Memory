12969bab9a33294e8d1f6c88fed7cfd9145f0036
divanoff <github@sonicware.org>
2018-06-04 00:42:59 -0400

    Added modal at the end of the game. Added timer

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 window.onload = loadGame;
 
-// Fisher-Yates shuffle function from https://bost.ocks.org/mike/shuffle/. Modified to use ES2015.
+/**
+ * Fisher-Yates shuffle function from https://bost.ocks.org/mike/shuffle/
+ * Modified to use ES2015 constructs
+ */
 function shuffle(array) {
     let curr = array.length,
         temp, rand;
@@ -15,6 +18,7 @@ function shuffle(array) {
 
 /*
  * Create a list that holds all of your cards
+ * My list actually holds the unique class name for each card
  */
 let cards = ['fa-diamond', 'fa-diamond', 'fa-paper-plane-o', 'fa-paper-plane-o', 'fa-anchor', 'fa-anchor', 'fa-bolt', 'fa-bolt', 'fa-cube', 'fa-cube', 'fa-leaf', 'fa-leaf', 'fa-bicycle', 'fa-bicycle', 'fa-bomb', 'fa-bomb'];
 
@@ -49,27 +53,42 @@ function loadGame() {
  */
 
 let deck = document.querySelector('ul.deck'),
-    openCards = [],
-    remainingCards = 16,
-    moves = 0;
+    restart = document.querySelector('.restart'),
+    openCards = [], // store the open card(s) after each click
+    remainingPairs = 8, // keep track of remaining unmatched pairs
+    moves = 0,
+    startTime,
+    endTime,
+    timerStarted = false,
+    timer;
 
 deck.addEventListener('click', function (e) {
-    // console.log(e.target.innerHTML);
+    // It will only fire if user clicks on an unopened card and there is less than 2 open cards
     if (e.target.classList.contains('show') ||
         e.target.classList.contains('open') ||
         openCards.length > 1 ||
         e.target.tagName == 'UL') {
         return;
     }
+
+    if (!timerStarted) {
+        startTimer();
+    }
+
     turnOver(e.target);
     addToOpen(e.target);
 });
 
-document.querySelector('.restart').addEventListener('click', function () {
+restart.addEventListener('click', function () {
     deck.innerHTML = '';
     moves = 0;
+    remainingPairs = 8;
     document.querySelector('.moves').textContent = moves;
-    document.querySelector('.stars').innerHTML = '<li><i class="fa fa-star"></i></li><li><i class="fa fa-star"></i></li><li><i class="fa fa-star"></i></li><li><i class="fa fa-star"></i></li><li><i class="fa fa-star"></i></li>';
+    document.querySelector('.stars').innerHTML = '';
+    for (let i = 0; i < 5; i++) {
+        document.querySelector('.stars').innerHTML += '<li>\n<i class="fa fa-star"></i>\n</li>';
+    }
+    resetTimer();
     loadGame();
 });
 
@@ -84,13 +103,21 @@ function addToOpen(element) {
     }
 }
 
+/*
+ * Compare cards.
+ * - If they match - keep them open, decrement remaining pairs
+ * - If they were the last pair - end the game
+ * - If they don't match - close them
+ * - Empty the open cards array and update the move counter
+ */
 function compareCards() {
     if (openCards[0].innerHTML === openCards[1].innerHTML) {
         for (let card of openCards) {
             card.classList.toggle('match');
         }
-        remainingCards -= 2;
-        if (remainingCards === 0) {
+        remainingPairs--;
+        if (remainingPairs === 0) {
+            stopTimer();
             gameOver();
         }
     }
@@ -102,15 +129,79 @@ function compareCards() {
     updateCounter();
 }
 
+// Update the move counter and the star rating
 function updateCounter() {
     moves++;
     document.querySelector('.moves').textContent = moves;
 
-    if ([1, 2, 3, 4].includes(moves)) {
+    if ([10, 12, 15, 20].includes(moves)) {
         document.querySelector('.stars').firstElementChild.remove();
     }
 }
 
+/*
+ * End the game
+ * Build the modal and display it
+ * popup - semi-transparent overlay
+ * popMsg - modal
+ * If player selects "Yes" to play again the board, star rating, moves, and timer are all reset
+ * If player selects "No" the modal is removed and everything stays unchanged
+ */
 function gameOver() {
+    let popup = document.createElement('div'),
+        popMsg = document.createElement('div');
 
+    popup.classList.add('modal');
+    popMsg.classList.add('modal-message');
+    popMsg.innerHTML = `<h2>Congratulations!!!</h2>`;
+    popMsg.innerHTML += `<p>You won in ${moves} moves.</p>`;
+    popMsg.innerHTML += `<p>It took you ${Math.floor((endTime - startTime) / 60000)} minute(s) and ${Math.round(((endTime - startTime) / 1000) % 60)} second(s).</p>`
+    popMsg.innerHTML += `<p>Your rating is ${document.querySelector('.stars').children.length} stars.</p>`;
+    popMsg.innerHTML += `<h3>Do you want to play again?</h3>`;
+    popMsg.innerHTML += `<button class="modal-button yes">Yes</button>`;
+    popMsg.innerHTML += `<button class="modal-button no">No</button>`;
+
+    popup.appendChild(popMsg);
+    document.querySelector('.container').appendChild(popup);
+
+    document.querySelector('.yes').addEventListener('click', function () {
+        popup.remove();
+        deck.innerHTML = '';
+        loadGame();
+    });
+
+    document.querySelector('.no').addEventListener('click', function () {
+        popup.remove();
+    });
+}
+
+function startTimer() {
+    if (!timerStarted) {
+        timerStarted = true;
+        startTime = Date.now();
+        timer = setInterval(function () {
+            let minutes = Math.floor((Date.now() - startTime) / 60000),
+                seconds = Math.round(((Date.now() - startTime) / 1000) % 60);
+            minutes = minutes < 10 ? "0" + minutes : minutes;
+            seconds = seconds < 10 ? "0" + seconds : seconds;
+            document.querySelector('.timer').innerHTML = `${minutes}:${seconds}`;
+        }, 1000)
+    } else {
+        console.log('Timer already started');
+    }
+}
+
+function stopTimer() {
+    if (timerStarted) {
+        timerStarted = false;
+        endTime = Date.now();
+        clearInterval(timer);
+    } else {
+        console.log('Timer has not been started yet.');
+    }
+}
+
+function resetTimer() {
+    stopTimer();
+    document.querySelector('.timer').innerHTML = '00:00';
 }
